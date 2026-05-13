83f4bd5f6a7a9cd8463e7cf83c841aaee5e5ea5a
Leo Durnion <wbw517@york.ac.uk>
2026-05-13 23:54:24 +0100

    Updated to 4 layers, added pooling, moved ReLU

--- model.py
+++ model.py
@@ -5,31 +5,46 @@ import torch.nn as nn
 from torchvision.datasets import OxfordIIITPet
 import torch.optim as optim
 
-# importing/downloading all dataset images/labels
+'''
+Importing/downloading all dataset images/labels
+Download set to False as all images have been downloaded locally
+Input images: 224x224x3
+'''
 
 dataset = OxfordIIITPet(
     root="./data",
     download=False
 )
 
-class CNN(nn.module):
+class CNN(nn.Module):
     def __init__(self, num_classes=37):
         super().__init__()
 
-        # Convolutional layers
+        '''
+        4 blocks of convolution layers + pooling
+        + ReLU after layers
+        '''
+        # Block 1
 
         self.convolution1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+
+        # Block 2
 
         self.convolution2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2, stride=2)
+
+        # Block 3
 
         self.convolution3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(2, stride=2)
+
+        # Block 4
 
         self.convolution4 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.relu4 = nn.ReLU()
+        self.pool4 = nn.MaxPool2d(2, stride=2)
 
-        self.convolution5 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-        self.relu5 = nn.ReLU()
+        # ReLU - implement into the forward function
+
+        self.relu = nn.ReLU()
 
